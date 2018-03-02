8d80af26c431bc26ae9aef9dbd1369e025d75b7f
johndear <510671759@qq.com>
2018-03-02 15:34:18 +0800

    Update ReceiveLogs1.java
    消息确认机制

--- src/main/java/com/op/mq/fanout/ReceiveLogs1.java
+++ src/main/java/com/op/mq/fanout/ReceiveLogs1.java
@@ -31,5 +31,22 @@ public class ReceiveLogs1 {
 			}
 		};
 		channel.basicConsume(queueName, true, consumer);
+		
+		// 消息确认机制
+		//		Consumer consumer = new DefaultConsumer(channel) {
+//			@Override
+//			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+//				String message = new String(body, "UTF-8");
+//				try{
+//					System.out.println(" [x] Received '" + message + "'");
+//					// false：表示从队列中删除（包括Unacked和Total）
+//					channel.basicAck(envelope.getDeliveryTag(), false);
+//				} catch(Exception e){
+//					// true：表示将消息重新放入到队列中，false：表示直接从队列中删除(等同于：channel.basicAck(envelope.getDeliveryTag(), false);)
+//					channel.basicReject(envelope.getDeliveryTag(), true);
+//				}
+//			}
+//		};
+//		channel.basicConsume(queueName, false, consumer);
 	}
 }
